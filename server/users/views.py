7e614bac34767d643fdb8320d10196ecffe9e6cd
OZZO8 <ondra.zeman.koryta@seznam.cz>
2026-02-19 10:06:23 +0100

    chore: add temporary user endpoints for testing

--- server/users/views.py
+++ server/users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def users_view(request):
+
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
